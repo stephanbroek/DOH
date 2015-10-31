368b839cd5d0bfdd789068eeb89aec2df17b82a7
Stephan Broek <SABroek1@shef.ac.uk>
2015-10-31 03:45:45 +0100

    added resources and gitignore

---
+++ resources/headers.js
@@ -0,0 +1,6 @@
+// Headers for the request function calls.
+module.exports = {
+  'User-Agent': 'IW/0.0.1',
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'Accept': 'application/json'
+};
