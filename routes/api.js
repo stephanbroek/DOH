7a848e710c6339aacec946a0ba9a0fadda62bb5a
Stephan Broek <SABroek1@shef.ac.uk>
2015-10-31 21:21:14 +0100

    added exposure of philips sleep api

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var dateFormat = require('dateformat');
 var philipsAcc = require('../resources/philips');
 
 // mysql pool created and credentials stored in a resource file.
@@ -63,7 +64,7 @@ router.get('/test', function(req, res, next)
 {
   //some function
 
-  some_json = {body:"<div id=main>Test</div>",main: "New Main"};
+  some_json = {body:"<div id=main>Test</div>",main: "New Main", datetest: dateFormat(new Date(1446313748*1000), "yyyy-mm-dd'T'HH:mm"), timestamp: Math.floor(new Date() / 1000)};
 
   res.json(some_json);
 
@@ -77,6 +78,42 @@ router.get('/tokentest/:uid', function(req, res, next)
   });  
 });
 
-router.get('')
+router.get('/sleep/:uid', function(req, res, next)
+{
+  getAuthToken(req.params.uid, function(accesToken)
+  {
+    startTimestamp = req.query.start;
+    endTimestamp = req.query.end;
+    startPhilips = dateFormat(new Date(startTimestamp * 1000),"yyyy-mm-dd'T'HH:mm");
+    endPhilips = dateFormat(new Date(endTimestamp * 1000), "yyyy-mm-dd'T'HH:mm"); 
+    var options = {
+      url: "https://www.measuretomotivate.philips.com/api/fhir/Observation",
+      method: 'GET',
+      headers: headers,
+      qs: {"patient.id": req.params.uid, type: "sleepduration", start: startPhilips, end: endPhilips}
+    }
+    options.headers.Authorization = "bearer " + accesToken;
+    request(options, function(err, response, body)
+    {
+      if(err)
+      {
+        console.log(err);
+        return;
+      }
+      data = JSON.parse(body).entry
+      ret_res = [];
+      data.forEach(function(val, index, array)
+      {
+        ret_res.push(
+        {
+          start: val.content.appliesPeriod.start, 
+          end: val.content.appliesPeriod.end, 
+          length: val.content.valueQuantity.value/3600
+        });
+      });
+      res.json(ret_res);
+    });
+  });
+});
 
 module.exports = router;
